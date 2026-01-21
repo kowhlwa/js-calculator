b07a2b514f122d20e161d44d89eed197d4902209
kowhlwa <hyunssl101@gmail.com>
2026-01-20 22:18:11 -0500

    Added basic functions for adding, subtracting, multplying, and dividing.

---
+++ script.js
@@ -0,0 +1,13 @@
+// basic functions in calculator
+const add = function(a, b) {
+    return a + b;
+};
+const subtract = function(a, b) {
+    return a - b;
+};
+const multiply = function(a, b) {
+    return a * b;
+};
+const divide = function(a, b) {
+    return a / b;
+};
