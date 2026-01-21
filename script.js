758e3f479888d1c7f07dee3e5ac77597029c46be
kowhlwa <hyunssl101@gmail.com>
2026-01-20 22:22:10 -0500

    Added operate function that acts as a switch to all of the operations

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ const multiply = function(a, b) {
 const divide = function(a, b) {
     return a / b;
 };
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+const operate = function(first, second, operator) {
+    switch (operator) {
+        case "+":
+            return add(first, second);
+        case "-":
+            return subtract(first, second);
+        case "*":
+            return multiply(first, second);
+        default:
+            return divide(first, second);
+    }
+};
