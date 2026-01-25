e76c7e31d9bd4e81419b683344e6f57ff1c80f38
kowhlwa <hyunssl101@gmail.com>
2026-01-25 15:45:19 -0500

    Extremely buggy calculator logic implementation

--- script.js
+++ script.js
@@ -12,19 +12,91 @@ const divide = function(a, b) {
     return a / b;
 };
 
-let firstNumber;
-let secondNumber;
+let firstNumber = 0;
+let secondNumber = 0;
 let operator;
+let which = "first";
 
-const operate = function(first, second, operator) {
-    switch (operator) {
+// function to update the display
+const calcDisplay = document.querySelector(".calculator-display");
+function updateDisplay() {
+    if (which === "first") {
+        calcDisplay.textContent = firstNumber;
+    } else if (which === "op") {
+        calcDisplay.textContent = firstNumber + " " + operator;
+    } else {
+        calcDisplay.textContent = firstNumber + " " + operator + " " + secondNumber;
+    }
+}
+
+// function that runs when you press AC
+function clearDisplay() {
+    firstNumber = 0;
+    secondNumber = 0;
+    which = "first";
+    calcDisplay.textContent = 0;
+}
+
+const operate = function(first, second, op) {
+    switch (op) {
         case "+":
             return add(first, second);
         case "-":
             return subtract(first, second);
         case "*":
             return multiply(first, second);
-        default:
+        case "÷":
             return divide(first, second);
+        case "=":
+            return operate(first, second, operator);
+
     }
 };
+// Funtion to update globals
+function updateVal(val) {
+    if (val === "AC" || val === ".") {
+        clearDisplay();
+        return;
+    }
+    if (val === "=" && (which === "first" || which === "op")) {
+        return;
+    }
+    if (which === "first") {
+        if (+val || +val === 0) {
+            firstNumber *= 10;
+            firstNumber += +val;
+        } else {
+            operator = val;
+            which = "op";
+        }
+    } else if (which === "second") {
+        if (+val || +val === 0) {
+            secondNumber *= 10;
+            secondNumber += +val;
+        } else {
+            if (val !== "=") {
+                firstNumber = operate(firstNumber, secondNumber, operator);
+                operator = val;
+                which = "second";
+            } else {
+                firstNumber = operate(firstNumber, secondNumber, val);
+                which = "first";
+            }
+            secondNumber = 0;
+        }
+    } else {
+        if (+val) {
+            secondNumber = +val;
+            which = "second";
+        } else {
+            operator = val;
+        }
+    }
+    updateDisplay();
+}
+
+// Each digit button gets a function to update our globals
+const digitButtons = document.querySelectorAll(".calculator-buttons *");
+digitButtons.forEach((digitButton) => {
+    digitButton.addEventListener("click", (e) => updateVal(e.target.textContent));
+});
